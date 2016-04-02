a039bdcb52abbb9a564c83f716220ec2ad6c3dac
lyman wong <lymanwong@gmail.com>
2016-04-01 17:34:54 -0700

    added doc title

--- index.php
+++ index.php
@@ -1,32 +1,84 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title></title>
-    <meta name="description" content="Lyman Wong, Web Developer" />
+    <title>MCplusMusic - Professional Wedding MC & Live Music Entertainment - San Francisco and Bay Area</title>
+    <meta name="MCPLUSMUSIC Website" content="" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="generator" content="Codeply">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
     <link href="//cdnjs.cloudflare.com/ajax/libs/animate.css/3.1.1/animate.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" />
+    <!-- <link rel="stylesheet" href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" /> -->
     <link rel="stylesheet" href="css/styles.css" />
     <link rel="stylesheet" href="css/animate.min.css" />
-    <link rel="icon" type="image/png" href="http://lymanwong.com/images/favicon.ico" />
+    <link href="css/simple-sidebar.css" rel="stylesheet">
+    <link rel="icon" type="image/png" href="favicon.ico" />
+
   </head>
 </html>
 <body>
+<div id="wrapper">
+
+    <!-- Sidebar -->
+    <div id="sidebar-wrapper">
+        <ul class="sidebar-nav">
+            <li class="sidebar-brand">
+                <a href="#">
+                    MCplusMusic
+                </a>
+            </li>
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#">Bio</a>
+            </li>
+            <li>
+                <a href="#">Gallery</a>
+            </li>
+            <li>
+                <a href="#">Music</a>
+            </li>
+            <li>
+                <a href="#">Testimonials</a>
+            </li>
+            <li>
+                <a href="#">Contacts</a>
+            </li>
+        </ul>
+    </div>
+    <!-- /#sidebar-wrapper -->
+
+    <!-- Page Content -->
+    <div id="page-content-wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>Welcome to MCplusMusic</h1>
+
+                    <p>We are experienced with many aspects of entertainment and event hosting, and specialize in providing a variety of packages for weddings, birthdays /anniversaries, baby/bridal showers and corporate event services. MCplusMusic offers an individual approach to planning your special day with step-by-step guide, themed party ideas, unique music selection and an extensive preferred vendor list for all of your planning needs.</p>
+
+                    <p>Please contact us to schedule your complimentary consultation today and discuss all the details.</p>
+
+                    <p>We look forward to working with you!</p>
+
+                    <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Menu</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /#page-content-wrapper -->
+
+</div>
 
 
 
-
-
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
-    <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular.min.js"></script>
 <!--     <script src="//code.angularjs.org/1.2.0rc1/angular-route.js"></script>
-    <script src="//code.angularjs.org/1.2.0rc1/angular-animate.js"></script> -->
-    <script src="//cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.js"></script>
-    <script src="http://connect.soundcloud.com/sdk.js"></script>
-    <script src="js/scripts.js"></script>
-    <script src="js/sound.js"></script>
+<script src="//code.angularjs.org/1.2.0rc1/angular-animate.js"></script> -->
+<script src="//cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.js"></script>
+<script src="http://connect.soundcloud.com/sdk.js"></script>
+<script src="js/scripts.js"></script>
+<script src="js/sound.js"></script>
 </body>
