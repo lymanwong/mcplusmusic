96d1dff68716d20d8eff10a1c4b8343c1def27fa
lyman wong <lymanwong@gmail.com>
2016-04-01 17:06:19 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title></title>
+    <meta name="description" content="Lyman Wong, Web Developer" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="generator" content="Codeply">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
+    <link href="//cdnjs.cloudflare.com/ajax/libs/animate.css/3.1.1/animate.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" />
+    <link rel="stylesheet" href="css/styles.css" />
+    <link rel="stylesheet" href="css/animate.min.css" />
+    <link rel="icon" type="image/png" href="http://lymanwong.com/images/favicon.ico" />
+  </head>
+</html>
+<body>
+
+
+
+
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular.min.js"></script>
+<!--     <script src="//code.angularjs.org/1.2.0rc1/angular-route.js"></script>
+    <script src="//code.angularjs.org/1.2.0rc1/angular-animate.js"></script> -->
+    <script src="//cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.js"></script>
+    <script src="http://connect.soundcloud.com/sdk.js"></script>
+    <script src="js/scripts.js"></script>
+    <script src="js/sound.js"></script>
+</body>
