e39a26ad237b6543be1200d81070e63c3917b4c2
lyman@le <lymanwong@gmail.com>
2016-05-10 16:13:24 -0700

    update email href link

--- partials/footer.php
+++ partials/footer.php
@@ -1,4 +1,4 @@
 <footer>
-    <h4><a href="*">lenaoks@ymail.com</a></h4>
+    <h4><a href="mailto:lenaoks@ymail.com?Subject=Hello from your page">lenaoks@ymail.com</a></h4>
     <h4>(415) 309+9709</h4>
 </footer>
